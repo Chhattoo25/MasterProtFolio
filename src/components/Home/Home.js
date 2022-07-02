ac4dce2fa37f3de85925a2781b0729b00acab71d
Chhattoo25 <95302680+Chhattoo25@users.noreply.github.com>
2022-07-02 21:30:08 +0530

    done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -25,7 +25,7 @@ function Home() {
                 <strong className="main-name"> Chhatrapal Ahirwar</strong>
               </h1>
 
-              <div style={{ padding: 51, textAlign: "left" }}>
+              <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
             </Col>
